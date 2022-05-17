c6d73b1dd6ae9d9205981de160f52ae401db4003
Paxstan Cassal Paulson <paxstanfernandez24@gmail.com>
2022-05-17 12:21:26 +0200

    Initial project components

---
+++ dataflow-modeler/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+</template>
+
+<script>
+
+import SidePanelComponent from "@/components/SidePanelComponent";
+import canvasComponent from "@/components/CanvasComponent";
+import MenuComponent from "@/components/MenuComponent";
+
+export default {
+  name: 'App',
+  components: {
+    SidePanelComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
